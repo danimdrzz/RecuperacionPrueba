e18be154f5699e34e17ac3ca1c696577bd60b63c
MURILLO DIAZ-PINTADO <daniel.murillo1@isepceu.es>
2025-06-03 16:51:41 +0200

    Circulo

---
+++ src/Formas/Circulo.java
@@ -0,0 +1,60 @@
+package Formas;
+
+public class Circulo {
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the radio
+	 */
+	public double getRadio() {
+		return radio;
+	}
+	/**
+	 * @param radio the radio to set
+	 */
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+	private int id;
+	@Override
+	public String toString() {
+		return "Circulo [id=" + id + ", nombre=" + nombre + ", radio=" + radio + ", color=" + color + "]";
+	}
+	private String nombre;
+	private double radio;
+	private String color;
+}
